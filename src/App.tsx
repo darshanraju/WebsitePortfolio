f2e5b83bbac507e641f8998f8cd090f7874d3607
darshanraju <darshan.raju99@gmail.com>
2021-04-02 17:17:31 +1100

    Removed unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from "react";
 import "./App.css";
 import Main from "./components/main";
-import logo from "./AWSLogo.png";
 
 function App() {
   return (
     <div className="App">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
       <Main />
     </div>
   );
