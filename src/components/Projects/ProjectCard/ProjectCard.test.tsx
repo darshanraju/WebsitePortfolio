efc2cce02a1b769f767a2be425b3d17c04020b9c
darshanraju <darshan.raju99@gmail.com>
2021-04-13 07:56:01 +1000

    Added tests and test-id's for ProjectCards component

---
+++ src/components/Projects/ProjectCard/ProjectCard.test.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import ProjectCard, { ProjectCardData } from "./ProjectCard";
+import "@testing-library/jest-dom/extend-expect";
+const mockData: ProjectCardData = {
+  gitHubRepositoryLink: "https://www.google.com",
+  image:
+    "https://images.pexels.com/photos/6171709/pexels-photo-6171709.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  projectInfo: "darshan was here",
+  projectName: "mockName",
+  tools: ["React", "Typescript"],
+  liveProjectLink: "https://darshanraju.copm",
+};
+
+describe("ProjectCard", () => {
+  it("renders correctly", () => {
+    const { queryByTestId } = render(
+      <ProjectCard
+        gitHubRepositoryLink={mockData.gitHubRepositoryLink}
+        image={mockData.image}
+        projectInfo={mockData.projectInfo}
+        projectName={mockData.projectName}
+        tools={mockData.tools}
+        liveProjectLink={mockData.liveProjectLink}
+      />
+    );
+
+    expect(queryByTestId("project-card-container")).toBeTruthy();
+  });
+  it("clicking on project opens URL", () => {
+    const windowSpy = jest.spyOn(window, "open").mockImplementation(() => null);
+
+    const { queryByTestId, getByTestId } = render(
+      <ProjectCard
+        gitHubRepositoryLink={mockData.gitHubRepositoryLink}
+        image={mockData.image}
+        projectInfo={mockData.projectInfo}
+        projectName={mockData.projectName}
+        tools={mockData.tools}
+        liveProjectLink={mockData.liveProjectLink}
+      />
+    );
+
+    const projectImage = getByTestId("project-image");
+    fireEvent.click(projectImage);
+    expect(windowSpy).toBeCalledTimes(1);
+  });
+  it("clicking on gitHub opens repo", () => {
+    const windowSpy = jest.spyOn(window, "open").mockImplementation(() => null);
+
+    const { queryByTestId, getByTestId } = render(
+      <ProjectCard
+        gitHubRepositoryLink={mockData.gitHubRepositoryLink}
+        image={mockData.image}
+        projectInfo={mockData.projectInfo}
+        projectName={mockData.projectName}
+        tools={mockData.tools}
+        liveProjectLink={mockData.liveProjectLink}
+      />
+    );
+
+    const gitHubLink = getByTestId("github-link-icon");
+    fireEvent.click(gitHubLink);
+    expect(windowSpy).toBeCalledTimes(1);
+  });
+  it("project title is correct", () => {
+    const { queryByTestId } = render(
+      <ProjectCard
+        gitHubRepositoryLink={mockData.gitHubRepositoryLink}
+        image={mockData.image}
+        projectInfo={mockData.projectInfo}
+        projectName={mockData.projectName}
+        tools={mockData.tools}
+        liveProjectLink={mockData.liveProjectLink}
+      />
+    );
+
+    expect(queryByTestId("project-name")).toHaveTextContent(
+      mockData.projectName
+    );
+  });
+  it("project description is correct", () => {
+    const { queryByTestId } = render(
+      <ProjectCard
+        gitHubRepositoryLink={mockData.gitHubRepositoryLink}
+        image={mockData.image}
+        projectInfo={mockData.projectInfo}
+        projectName={mockData.projectName}
+        tools={mockData.tools}
+        liveProjectLink={mockData.liveProjectLink}
+      />
+    );
+
+    expect(queryByTestId("project-info")).toHaveTextContent(
+      mockData.projectInfo
+    );
+  });
+  it("project tools is correct", () => {
+    const { queryByTestId } = render(
+      <ProjectCard
+        gitHubRepositoryLink={mockData.gitHubRepositoryLink}
+        image={mockData.image}
+        projectInfo={mockData.projectInfo}
+        projectName={mockData.projectName}
+        tools={mockData.tools}
+        liveProjectLink={mockData.liveProjectLink}
+      />
+    );
+
+    const toolsContainer = queryByTestId("project-tools-container");
+
+    expect(toolsContainer?.childNodes.length).toBe(mockData.tools.length);
+
+    expect(toolsContainer).toHaveTextContent(mockData.tools.join(""));
+  });
+});
--- src/components/Projects/ProjectCard/ProjectCard.tsx
+++ src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -12,9 +12,13 @@ const ProjectCard = ({
   liveProjectLink,
 }: ProjectCardData) => {
   return (
-    <div className="project-card-container">
+    <div
+      className="project-card-container"
+      data-testid="project-card-container"
+    >
       <Fade duraction={800} left>
         <div
+          data-testid="project-image"
           onClick={() => window.open(liveProjectLink)}
           className="project-image-container"
         >
@@ -24,11 +28,18 @@ const ProjectCard = ({
 
       <Fade duraction={800} right>
         <div className="project-info-container">
-          <div className="project-name">{projectName}</div>
-          <div className="project-info-color-card">
-            <div className="project-info">{projectInfo}</div>
+          <div className="project-name" data-testid="project-name">
+            {projectName}
           </div>
-          <div className="project-tools-container">
+          <div className="project-info-color-card">
+            <div className="project-info" data-testid="project-info">
+              {projectInfo}
+            </div>
+          </div>
+          <div
+            className="project-tools-container"
+            data-testid="project-tools-container"
+          >
             {tools.map((tool, i) => {
               return (
                 <div key={i} className="project-tools">
@@ -39,6 +50,7 @@ const ProjectCard = ({
           </div>
           <div className="github-link">
             <i
+              data-testid="github-link-icon"
               onClick={() => window.open(gitHubRepositoryLink)}
               className="fab fa-github"
             ></i>
@@ -49,7 +61,7 @@ const ProjectCard = ({
   );
 };
 
-interface ProjectCardData {
+export interface ProjectCardData {
   gitHubRepositoryLink: string;
   liveProjectLink?: string;
   image: any;
