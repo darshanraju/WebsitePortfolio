96001224b995d4c05218c82db590bd4b21d0781e
darshanraju <darshan.raju99@gmail.com>
2021-04-14 15:06:57 +1000

    removing comments

--- src/components/Work/WorkPage/WorkPage.tsx
+++ src/components/Work/WorkPage/WorkPage.tsx
@@ -21,7 +21,6 @@ const WorkPage: React.FC = () => {
         <span className="green">01. </span>Experience
       </div>
       <div className="exp-card-container">
-        {/* <div className="card-image-co"> */}
         {experiences.map((exp, index) => {
           const companyMatcher = new RegExp(exp.iconName, "i");
           const logoV2 = logos.find((logo: any) => {
@@ -42,44 +41,6 @@ const WorkPage: React.FC = () => {
             </div>
           );
         })}
-        {/* </div> */}
-
-        {/* <div>
-          <Fade duration={500} right>
-            <div className="aws-image" />
-          </Fade>
-          <WordCard
-            role="Software Development Engineering Intern"
-            duration="Nov 2020 - Feb 2021"
-            company="Amazon Web Services"
-            responsibilities={[
-              "Used Typescript with React to develop and maintain features in an AWS service.",
-              "Integrated S3 into a new AWS Incident Management service, enabling customers to upload files to incidents, which allows for the documenting ofprogression and resolution or incidents.",
-            ]}
-            key={1}
-          />
-        </div>
-        <div>
-          <Fade duration={500} left>
-            <div className="serviceNow-image"></div>
-          </Fade>
-
-          <WordCard
-            role="Site Reliability Engineering Intern"
-            duration="Nov 2020 - Feb 2021"
-            company="ServiceNow"
-            responsibilities={[
-              "Maintained customer instances by \
-monitoring availability and latency.",
-              "Utilise MySQL, MariaDB and the InnoDB engine to resolve database monitoring\
-          alerts.",
-              "Operated in CentOS to analyse OS monitoring alerts.",
-              "Used automations runbooks to provide minimal mean time to resolutions\
-          during outages.",
-            ]}
-            key={2}
-          />
-        </div> */}
       </div>
     </section>
   );
